80e1f9e90c64e51994b4be2b918e23a5c8156fef
managata <37179233+managata@users.noreply.github.com>
2018-09-01 13:03:34 +0900

    add '--delete' for default rsync option

--- option.go
+++ option.go
@@ -28,7 +28,7 @@ type Options struct {
 	//	SshOption  []string `short:"b" long:"ssh-option" description:"ssh options" default:"{\"-l\", \"mana\"}"`
 
 	RsyncCommand string   `short:"x" long:"rsync-command" description:"rsync command" default:"rsync"`
-	RsyncOptions []string `short:"y" long:"rsync-option" description:"rsync options" default:"-dgloptADHX"`
+	RsyncOptions []string `short:"y" long:"rsync-option" description:"rsync options" default:"-dgloptADHX" default:"--delete"`
 
 	//
 
